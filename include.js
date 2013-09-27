6bac11a9b693324b6937a6086a384a6977608d1c
Jérémy Barbe <capitaine.mousse@gmail.com>
2013-09-27 17:12:18 +0200

    Fix an error when trying to reload http module Fix var name

--- include.js
+++ include.js
@@ -61,11 +61,11 @@
                 exec         = module[2],
                 args         = [];
 
-            each(dependencies, function (dependencie, name) {
-                name = dependencie.push ? dependencie[0] : dependencie
+            each(dependencies, function (dependencie, n) {
+                n = dependencie.push ? dependencie[0] : dependencie
 
-                if (modules[name] !== undefined) {
-                    args.push(modules[name]);
+                if (modules[n] !== undefined) {
+                    args.push(modules[n]);
                 }
             });
 
@@ -111,9 +111,9 @@
         }
 
         // Is this script add a waiting module ? If not, that's a "normal" script file
-        if (count > waitingModules.length) {
+        if (count > waitingModules.length && !name) {
             modules[name] = scriptCounter--;
-        } else if (waitingModules[0][0] === null) {
+        } else if (waitingModules.length && waitingModules[0][0] === null) {
             waitingModules[0][0] = name;
         }
 
