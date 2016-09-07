d887ab4ec255ca5c54677eece188a216cc9e9dfc
Jérémy Barbe <capitaine.mousse@gmail.com>
2016-09-07 11:29:25 +0200

    Add css loading. Major bugfixes

--- include.js
+++ include.js
@@ -1,6 +1,6 @@
 // =============================================================================
 // Project:        Include.js - Javascript loader
-// Copyright:      ©2011-2013 Jérémy Barbe (http://shwaark.com) and contributors
+// Copyright:      ©2011-2013 Jérémy Barbe (http://jeremy.sh) and contributors
 // Licence:        Licence under MIT license
 // =============================================================================
 
@@ -36,184 +36,10 @@
      */
     var head = document.getElementsByTagName('head')[0];
 
-    /**
-     * Loop trougth an array of element with the given function
-     * @param  {Array|NodeList}    array    array to loop       
-     * @param  {Function} callback function to execute with each element
-     */
-    function each(array, callback) {
-        var i;
 
-        for (i = 0; i < array.length; i++) {
-            if (array[i] !== undefined && callback(array[i], i, array) === false) {
-                break;
-            }
-        }
-    }
+    function Include (name, deps, module) {
+        var self = this;
 
-    /**
-     * Check if a module is loaded
-     */
-    function checkModuleLoaded() {
-        each(waitingModules, function (module, i) {
-            var name         = module[0],
-                dependencies = module[1],
-                exec         = module[2],
-                args         = [];
-
-            each(dependencies, function (dependencie, n) {
-                n = dependencie.push ? dependencie[0] : dependencie
-
-                if (modules[n] !== undefined) {
-                    args.push(modules[n]);
-                }
-            });
-
-            if (dependencies.length === args.length || dependencies.length === 0) {
-                exec = typeof exec == 'function' ? exec.apply(this, args) : exec;
-                module.push(true);
-
-                if (name !== null) {
-                    modules[name] = exec;
-                }
-
-                if (name === null && i+1 === waitingModules.length) {
-                    waitingModules = [];
-                    scriptCounter = 1;
-                }
-            }
-        });
-    }
-
-    /**
-     * On Load event
-     * @param  {Event}      event  event of the load
-     */
-    function onLoad(event) {
-        var target = (event.currentTarget || event.srcElement),
-            name,
-            count;
-
-        //Check if the script is realy loaded and executed ! (Fuck you IE with your "Loaded but not realy, wait to be completed")
-        if (event.type !== "load" && target.readyState != "complete") {
-            return;
-        }
-
-        name = target.getAttribute('data-module');
-        count = target.getAttribute('data-count');
-        target.setAttribute('data-loaded', true);
-
-        // Old browser need to use the detachEvent method
-        if (target.attachEvent) {
-            target.detachEvent('onreadystatechange', onLoad);
-        } else {
-            target.removeEventListener('load', onLoad);
-        }
-
-        // Is this script add a waiting module ? If not, that's a "normal" script file
-        if (count > waitingModules.length) {
-            scriptCounter--;
-            modules[name] = modules[name] || scriptCounter;
-        } else if (waitingModules[0][0] === null) {
-            waitingModules[0][0] = name;
-        }
-
-        checkModuleLoaded();
-    }
-
-    /**
-     * Attach events to a script tags
-     * @param {Element} script     script to attach event
-     */
-    function attachEvents(script) {
-        if (script.attachEvent) {
-            script.attachEvent('onreadystatechange', onLoad);
-        } else {
-            script.addEventListener('load', onLoad, false);
-        }
-    }
-
-    /**
-     * Check if a script already load
-     * @param  {String} moduleName module to load
-     */
-    function checkScripts(moduleName) {
-        var script = false;
-
-        each(document.getElementsByTagName('script'), function (elem) {
-            if (elem.getAttribute('data-module') && elem.getAttribute('data-module') === moduleName) {
-                script = elem;
-                return false;
-            }
-        });
-
-        return script;
-    }
-
-    /**
-     * Create a script element to load asked module
-     * @param  {String} moduleName name of the module
-     * @param  {String} moduleFile fiel to include
-     */
-    function create(moduleName, moduleFile) {
-        //SetTimeout prevent the "OMG RUN, CREATE THE SCRIPT ELEMENT, YOU FOOL" browser rush
-        setTimeout(function(){
-            var script = checkScripts(moduleName);
-            if (script) {
-                return;
-            }
-
-            scriptCounter++;
-
-            script = document.createElement('script');
-
-            script.async = true;
-            script.type = "text/javascript";
-            script.src = moduleFile;
-            script.setAttribute('data-module', moduleName);
-            script.setAttribute('data-count',  scriptCounter);
-            script.setAttribute('data-loaded', false);
-
-            if (baseElement) {
-                //prevent IE 6-8 bug (script executed before appenchild execution. Yeah, that's realy SUCK)
-                baseElement.parentNode.insertBefore(script, baseElement);
-            } else {
-                head.appendChild(script);
-            }
-
-            attachEvents(script);
-        }, 0);
-    }
-
-    /**
-     * Parse a file to include
-     * @param  {String} file  file to parse
-     */
-    function parseFiles(file) {
-        var moduleName = file.push ? file[0] : file;
-        var moduleFile = file.push ? file[1] : file;
-
-        //Don't load module already loaded
-        if (modules[moduleName]) {
-            checkModuleLoaded();
-            return;
-        }
-        
-
-        if (!/\.js/.test(moduleFile) && !/^http/.test(moduleFile)) {
-            moduleFile = moduleFile.replace('.', '/');
-            moduleFile = moduleFile + '.js';
-        }
-
-        create(moduleName, moduleFile);
-    }
-
-    /**
-     * @param {String}   name     the name of the module
-     * @param {Array}    deps     dependencies of the module
-     * @param {Function} module   module definition
-     */
-    environment['include'] = environment['require'] = environment['define'] = function (name, deps, module) {
         if (typeof name !== "string") {
             module = deps;
             deps = name;
@@ -227,11 +53,273 @@
 
         waitingModules.unshift([name, deps, module]);
 
-        checkModuleLoaded();
+        self.checkModuleLoaded();
 
         if (deps.length) {
-            each(deps, parseFiles);
+            self.each(deps, self.parseFiles);
         }
+    };
+
+    /**
+     * Loop trougth an array of element with the given function
+     * @param  {Array|NodeList}    array    array to loop
+     * @param  {Function} callback function to execute with each element
+     */
+    Include.prototype.each = function (array, callback) {
+        var self = this,
+            i;
+
+        for (i = 0; i < array.length; i++) {
+            if (array[i] !== undefined && callback.call(self, array[i], i, array) === false) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * Check if a module is loaded
+     */
+    Include.prototype.checkModuleLoaded = function () {
+        var self = this;
+
+        self.each(waitingModules, function (module, i) {
+            var name         = module[0],
+                dependencies = module[1],
+                exec         = module[2],
+                args         = [];
+
+            self.each(dependencies, function (dependencie, n, t) {
+                n = dependencie.push ? dependencie[0] : dependencie;
+                t = document.querySelector('[data-module="'+n+'"]');
+
+                if (modules[n] !== undefined) {
+                    args.push(modules[n]);
+                }
+
+                if (t && t.nodeName == "LINK") {
+                    args.push(null);
+                }
+            });
+
+            if (dependencies.length === args.length || dependencies.length === 0) {
+                if (name === null && i+1 === waitingModules.length) {
+                    waitingModules = [];
+                    scriptCounter = 1;
+                }
+
+                exec = typeof exec == 'function' ? exec.apply(this, args) : exec;
+
+                if (name !== null) {
+                    modules[name] = exec;
+                }
+            }
+        });
+    }
+
+    /**
+     * onModuleLoaded
+     * @param  {String}  name  name of the module
+     * @param  {Number}  index index of the module
+     * @param  {Boolean} isCss
+     */
+    Include.prototype.onModuleLoaded = function (name, index, isCss) {
+        var self = this;
+
+        // Is this script add a waiting module ? If not, that's a "normal" script file
+        if (index > waitingModules.length) {
+            scriptCounter--;
+            modules[name] = modules[name] || scriptCounter;
+        } else if (waitingModules[0][0] === null) {
+            waitingModules[0][0] = name;
+        }
+
+        self.checkModuleLoaded();
+    }
+
+    /**
+     * On Load event
+     * @param {Event}    event  event of the load
+     * @param {Function} caller
+     */
+    Include.prototype.onLoad = function (event, caller) {
+        var self = this,
+            target = (event.currentTarget || event.srcElement);
+
+        //Check if the script is realy loaded and executed
+        if (event.type !== "load" && target.readyState != "complete") {
+            return;
+        }
+
+        target.setAttribute('data-loaded', true);
+        self.onModuleLoaded(target.getAttribute('data-module'), target.getAttribute('data-count'));
+
+        // Old browser need to use the detachEvent method
+        if (target.attachEvent) {
+            target.detachEvent('onreadystatechange', caller);
+        } else {
+            target.removeEventListener('load', caller);
+        }
+    }
+
+    /**
+     * Watch for css load
+     * @param  {Element} elem elem to check loading
+     */
+    Include.prototype.watchCss = function (elem) {
+        var self = this,
+            sheets = document.styleSheets,
+            i = sheets.length,
+            href = elem.href.split('//').pop();
+
+        // loop on document stylesheets to check if media is loaded
+        while (i--) {
+            if (sheets[i].href.indexOf(href) != -1) {
+                elem.setAttribute('data-loaded', true);
+                self.onModuleLoaded(elem.getAttribute('data-module'), elem.getAttribute('data-count'), true);
+
+                self.checkModuleLoaded();
+                return;
+            }
+        }
+
+        setTimeout(function () {
+            self.watchCss.call(self, elem);
+        });
+    }
+
+    /**
+     * Attach events to an element
+     * @param {Element} elem     elem to attach event
+     * @param {Boolean} isJs     is elem a script
+     */
+    Include.prototype.attachEvents = function (elem, isJs) {
+        var self = this,
+            cb = function () {
+                var args = Array.prototype.slice.call(arguments);
+                args.push(cb);
+
+                self.onLoad.apply(self, args);
+            };
+
+
+        if (isJs) {
+            if (elem.attachEvent) {
+                elem.attachEvent('onreadystatechange', cb);
+            } else {
+                elem.addEventListener('load', cb, true);
+            }
+        } else {
+            self.watchCss(elem);
+        }
+    }
+
+    /**
+     * Check if a script already load
+     * @param  {String} moduleName module to load
+     * @param  {String} isJs       type of file
+     */
+    Include.prototype.checkExists = function (moduleName, isJs) {
+        var exists = false;
+
+        this.each(document.getElementsByTagName(isJs ? 'script' : 'link'), function (elem) {
+            if (elem.getAttribute('data-module') && elem.getAttribute('data-module') === moduleName) {
+                exists = elem;
+                return false;
+            }
+        });
+
+        return exists;
+    }
+
+    /**
+     * Create a script element to load asked module
+     * @param  {String} moduleName name of the module
+     * @param  {String} moduleFile file to include
+     * @param  {String} isJs       type of file
+     */
+    Include.prototype.create = function (moduleName, moduleFile, isJs) {
+        var self = this;
+
+        //SetTimeout prevent the element create browser rush
+        setTimeout(function(){
+            var elem = self.checkExists.call(self, moduleName, isJs);
+
+            if (elem) {
+                return;
+            }
+
+            scriptCounter++;
+
+            elem = document.createElement(isJs ? 'script' : 'link');
+
+            if (isJs) {
+                elem.async = true;
+                elem.type = "text/javascript";
+                elem.src = moduleFile;
+            } else {
+                elem.media = "all";
+                elem.href = moduleFile;
+                elem.rel = "stylesheet"
+            }
+
+            elem.setAttribute('data-module', moduleName);
+            elem.setAttribute('data-count',  scriptCounter);
+            elem.setAttribute('data-loaded', false);
+
+            if (baseElement) {
+                //prevent IE 6-8 bug (script executed before appenchild execution.
+                baseElement.parentNode.insertBefore(elem, baseElement);
+            } else {
+                head.appendChild(elem);
+            }
+
+            self.attachEvents.call(self, elem, isJs);
+        }, 0);
+    }
+
+    /**
+     * Parse a file to include
+     * @param  {String} file  file to parse
+     */
+    Include.prototype.parseFiles = function (file) {
+        var moduleName = file.push ? file[0] : file,
+            moduleFile = file.push ? file[1] : file,
+            ext;
+
+        //Don't load module already loaded
+        if (modules[moduleName]) {
+            this.checkModuleLoaded();
+            return;
+        }
+
+
+        if (moduleFile.indexOf('//') == -1 && !/\.js/.test(moduleFile) && !/^http/.test(moduleFile)) {
+            moduleFile = moduleFile.replace('.', '/');
+            moduleFile = moduleFile + '.js';
+        }
+
+        ext = moduleFile.split('.').pop() == 'js';
+
+        this.create.call(this, moduleName, moduleFile, ext);
+    }
+
+    /**
+     * Reset include
+     */
+    Include.prototype.reset = function () {
+        modules = {};
+        waitingModules = [];
+        scriptCounter = 1;
+    }
+
+    /**
+     * @param {String}   name     the name of the module
+     * @param {Array}    deps     dependencies of the module
+     * @param {Function} module   module definition
+     */
+    environment['include'] = environment['require'] = environment['define'] = function (name, deps, module) {
+        console.log(waitingModules);
+        return new Include(name, deps, module);
     };
 
 })(this);
